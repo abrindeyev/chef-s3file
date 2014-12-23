5abf37e7720bfcbcdf2c15981b102d3134c0c646
Andrey Brindeyev <abrindeyev@griddynamics.com>
2014-12-22 23:25:50 -0800

    name attribute is required starting from Chef 12

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,6 @@
+name             "chef-s3file"
 maintainer       "Robert J. Berger Runa, Inc"
 maintainer_email "ops@runa.com"
 license          "Apache 2.0"
 description      "Library to add s3 remote file to Chef DSL"
-version          "0.2.0"
+version          "0.2.1"
